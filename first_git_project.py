6ebbe609a5459eb59ecea57a8859fac69596df5f
Justin <justinwademan@gmail.com>
2022-02-04 14:42:27 -0500

    How to change unix prompt colors

--- first_git_project.py
+++ first_git_project.py
@@ -5,4 +5,12 @@ print("more added")
 print("g gggggggggggggggggggggggggggggggggggggggg")
 
 
+string = " PS1='\e[1;32m\u\e[0m @ \e[1;36m\W\e[0m$'"
+
+
+print("this is what you need to type to change unix prompt colors: " + string)
+
+
+
+
 
